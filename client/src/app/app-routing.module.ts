54845d3496509bf1036acdc35d257aaa7f7a40a9
Mari <palmer.m07@gmail.com>
2020-11-11 01:36:33 -0500

    worked on creating and editing survey

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CreateSurveyComponent } from './survey-library/create-survey/create-survey.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent, data: {title: 'Home'}},
+  {path: 'create', component: CreateSurveyComponent, data: {title: 'Create Survey'}},
   {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
---
+++ client/src/app/survey-library/question-form/question-form.component.ts
@@ -0,0 +1,55 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Question } from 'src/app/interfaces';
+
+@Component({
+  selector: 'app-question-form',
+  templateUrl: './question-form.component.html',
+  styleUrls: ['./question-form.component.css']
+})
+export class QuestionFormComponent implements OnInit, OnChanges {
+  @Input() question: Question;
+  public placeholderQuestion: Question;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.initializePlaceholderQuestion();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.question) {
+      this.placeholderQuestion = this.question;
+    }
+  }
+
+  onAddOption(): void {
+    this.placeholderQuestion.options.push({
+      details: ''
+    });
+  }
+
+  onDelete(index: number): void {
+    if (this.placeholderQuestion.options.length > 1)
+    {
+      this.placeholderQuestion.options.splice(index, index + 1);
+    }
+  }
+
+  // save edits
+  onSave(): void {
+    // TODO: do api to update
+    console.log(this.placeholderQuestion);
+
+    // reset form input
+    this.question = undefined;
+    this.initializePlaceholderQuestion();
+  }
+
+  initializePlaceholderQuestion(): void {
+    this.placeholderQuestion = {
+      title: '',
+      options: [{
+        details: ''
+      }]
+    };
+  }
+}
